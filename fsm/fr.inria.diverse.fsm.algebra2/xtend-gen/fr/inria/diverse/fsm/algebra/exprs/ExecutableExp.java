dae1b64e384617f4de01a3e13c4a4c116d21e006
Manuel Leduc <manuel.leduc@inria.fr>
2016-12-09 10:21:27 +0100

    cleanup xtend-gen versionned

--- fsm/fr.inria.diverse.fsm.algebra2/xtend-gen/fr/inria/diverse/fsm/algebra/exprs/ExecutableExp.java
+++
@@ -1,6 +0,0 @@
-package fr.inria.diverse.fsm.algebra.exprs;
-
-@SuppressWarnings("all")
-public interface ExecutableExp {
-  public abstract void execute();
-}
--- fsm/fr.inria.diverse.fsm.algebra2/xtend-gen/fr/inria/diverse/fsm/algebra/exprs/GraphvizExp.java
+++
@@ -1,6 +0,0 @@
-package fr.inria.diverse.fsm.algebra.exprs;
-
-@SuppressWarnings("all")
-public interface GraphvizExp {
-  public abstract String evalGraph();
-}
